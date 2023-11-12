48a1d78bc097b4287b048d0b891294462defe718
Royal <royalhuseyn902@gmail.com>
2023-11-12 23:08:42 +0400

    Get form data to use later with FormData constructor

--- script.js
+++ script.js
@@ -13,9 +13,19 @@ function showDialog() {
   formDialog.showModal();
 }
 
+function submitFormData(event) {
+  event.preventDefault();
+  formData = new FormData(form);
+  formDialog.close();
+}
+
 const myLibrary = [];
 
+let formData;
 const formDialog = document.querySelector("#form-dialog");
 const addNewBookBtn = document.querySelector(".add-book");
+const submitBtn = document.getElementById("submitBtn");
+const form = document.querySelector("#form-dialog > form");
 
 addNewBookBtn.addEventListener("click", showDialog);
+submitBtn.addEventListener("click", submitFormData);
